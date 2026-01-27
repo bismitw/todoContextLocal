a4e714bc326ca308bec22e712ab2aea89394b28b
Bismit Wagle <bismitwagle@gmail.com>
2026-01-27 17:10:08 +0530

    Feat: Create TodoContext

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,14 @@
+import {createContext, useContext} from "react"
+
+export const TodoContext = createContext({
+    todos:[{
+        id: 1,
+        todo: 'Todo Message',
+        completed: false
+    }
+    ],
+    addTodo: (todo) => {},
+    updateTodo:(id, todo) =>{},
+    deleteTodo:(id) =>{},
+    toggleComplete: (id) => {}
+})
