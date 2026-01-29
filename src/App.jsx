31a46249506dd0b565429b2f129404ee532358a7
Bismit Wagle <bismitwagle@gmail.com>
2026-01-29 14:42:31 +0530

    feat: useEffect hook for getting and setting todos from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './contexts'
 
 function App() {
-  const [todo, setTodos] = useState([])
+  const [todos, setTodos] = useState([])
   const addTodo = (todo) => {
-    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev])
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
   }
 
   const updateTodo = (id, todo) => {
@@ -18,6 +18,24 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
+  const toggleComplete = (id) => {
+    setTodos((prev)=>
+      prev.map((prevTodo) =>
+        prevTodo.id === id? {...prevTodo, completed: !prevTodo.completed}:{prevTodo})
+    )
+  }
+
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos'))
+    if(todos && todos.length<0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() =>{
+    localStorage.setItem('todos', JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
