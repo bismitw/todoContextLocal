f8c7919bf0d39a7c1c9d6d48af7337316f399958
Bismit Wagle <bismitwagle@gmail.com>
2026-01-29 14:29:30 +0530

    feat: import todoProvider to app.jsx and declare the functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodos] = useState([])
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
 
   return (
-    <>
-    
-    </>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    <h1 className='text-3xl font-bold underline'>
+      Hello World
+    </h1>
+    </TodoProvider>
   )
 }
 
